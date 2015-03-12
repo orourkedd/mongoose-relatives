eea8063af4268bb89be7e981da0bbe81b53fe74f
Frankie O'Rourke <frankie@orourkedd.com>
2015-03-13 00:40:02 +0300

    Update

--- index.js
+++ index.js
@@ -1,66 +1,79 @@
 var async = require('async');
+var _ = require('lodash');
+var Relationship = require('./relationship');
 
 module.exports = function (model, config) {
 
-	if (!config.property) {
-		throw new Error('property required.');
+	//Add helper methods
+	model.prototype.relationship = function (path) {
+		return new Relationship(this, path, config);
+	};
+
+	//Add Middleware
+	for (var p in config) {
+		var pathConfig = config[p];
+
+		//create middleware in closure for easy access to pathConfig
+		(function (path, pathConfig) {
+
+			model.schema.pre('save', function (next) {
+				var self = this;
+
+				if (!self[path]) {
+					return next();
+				}
+
+				var collection = self[path].toObject();
+				var setters = [];
+
+				collection.forEach(function (item, i) {
+					if (item.__processed) {
+						return;
+					}
+
+					setters.push((function (item, i) {
+						//Get the actual relationship object, not a copy
+						item = self[path][i];
+
+						return function (done) {
+							pathConfig.targetModel.findOne({
+									_id: item._id
+								},
+								function (err, doc) {
+									if (err) {
+										return done(err);
+									}
+
+									if (!doc) {
+										return done(new Error('Relationship points to document that does not exist.'));
+									}
+
+									//update paths
+									for (var j in pathConfig.paths || []) {
+										item[pathConfig.paths[j]] = doc[pathConfig.paths[j]]
+									}
+
+									//mark as processed so this doesn't happen on every save
+									item.__processed = (new Date()).getTime();
+
+									return done();
+								}
+							)
+						};
+					})(item, i));
+
+				});
+
+				async.parallel(setters, function (err, results) {
+					if (err) {
+						return next(err);
+					} else {
+						self.markModified(path);
+						next();
+					}
+				});
+			});
+		})(p, pathConfig);
+
 	}
-
-	model.schema.pre('save', function (next) {
-		var self = this;
-
-		if (!self[config.property]) {
-			return next();
-		}
-
-		var collection = self[config.property].toObject();
-		var setters = []
-
-		collection.forEach(function (item, i) {
-			if (!item.__processed) {
-				return;
-			}
-
-			setters.push((function (item, i) {
-				//Get the actual relationship object, not a copy
-				item = self[config.property][i];
-
-				return function (done) {
-					config.targetModel.findOne({
-							_id: item._id
-						},
-						function (err, doc) {
-							if (err) {
-								return done(err);
-							}
-
-							if (!doc) {
-								return done(new Error('Relationship points to document that does not exist.'));
-							}
-
-							//update properties
-							for (var j in config.properties || []) {
-								item[config.properties[j]] = doc[config.properties[j]]
-							}
-
-							//mark as processed so this doesn't happen on every save
-							item.__processed = (new Date()).getTime();
-
-							return done();
-						}
-					)
-				};
-			})(item, i));
-
-		});
-
-		async.parallel(setters, function (err, results) {
-			if (err) {
-				return next(err);
-			} else {
-				self.markModified(config.property);
-				next();
-			}
-		});
-	});
 };
---
+++ relationship.js
@@ -0,0 +1,61 @@
+var Relationship = function (resource, path, config) {
+	if (!config[path]) {
+		throw new Error('no config for path');
+	}
+
+	this.config = config[path];
+	this.resource = resource;
+	this.path = path;
+};
+
+Relationship.prototype.add = function (relationship) {
+	if (!relationship._id) {
+		throw new Error('_id is required for a valid relationship');
+	}
+
+	//Make sure this does not already exist in relationship
+	for (var i in this.resource[this.path].toObject()) {
+		if (this.resource[this.path][i]._id.toString() == relationship._id.toString()) {
+			return;
+			//throw new Error('A relationship already exists for this target.')
+		}
+	}
+
+	this.resource[this.path].push(relationship);
+};
+
+Relationship.prototype.remove = function (relationship) {
+	if (!relationship._id) {
+		throw new Error('_id is required for a valid relationship');
+	}
+
+	for (var i in this.resource[this.path].toObject()) {
+		if (this.resource[this.path][i]._id.toString() == relationship._id.toString()) {
+			this.resource[this.path].splice(i, 1);
+			break;
+		}
+	}
+};
+
+Relationship.prototype.update = function (relationship) {
+	if (!relationship._id) {
+		throw new Error('_id is required for a valid relationship');
+	}
+
+	var index = null;
+	//Make sure this does not already exist in relationship
+	for (var i in this.resource[this.path].toObject()) {
+		if (this.resource[this.path][i]._id.toString() == relationship._id.toString()) {
+			index = i;
+			break;
+		}
+	}
+
+	if (!index) {
+		throw new Error('Relationship does not exist.')
+	}
+
+	_.extend(this.resource[this.path][index], relationship);
+};
+
+module.exports = Relationship;
